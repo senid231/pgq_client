022dbe4a8c7e9de8df3894c61c2e5ab09ddca8ef
Denis Talakevich <senid231@gmail.com>
2020-09-19 15:02:00 +0300

    initial commit

---
+++ spec/pgq_client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pgq do
+  it 'has a version number' do
+    expect(Pgq::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
